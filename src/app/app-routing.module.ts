6b8eb0b3e9f462669fc0137a38a8600fb67932a0
Daniel Guimarães de Faria <danielguimaraesfaria@gmail.com>
2025-04-19 23:44:35 -0300

    Criando o card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardHomeComponent } from './modules/home/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
+  //* Algo Temporario para chamar a dash
+  { path: 'dash', component: DashboardHomeComponent },
   {
     path: 'login',
     loadChildren: () =>
